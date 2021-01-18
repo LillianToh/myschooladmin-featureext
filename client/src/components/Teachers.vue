b13cceef233462cb69c7c8e73fc3cb2712169a57
LillianToh <56778700+LillianToh@users.noreply.github.com>
2021-01-18 16:19:38 +0800

    tidy up code

--- client/src/components/Teachers.vue
+++ client/src/components/Teachers.vue
@@ -3,25 +3,27 @@
         <div>
             <div>
                 <ul class="list-group">
-                    <li v-b-toggle.collapse-1 variant="primary" class="list-group-item list-group-item-action" v-for="(teacher) in teachers" :key="teacher.id" @click="getSubjectsbyTeacherId(teacher.id); getStudentsbyTeacherId(teacher.id)">
+                    <li v-b-toggle.collapse-1 variant="primary" class="list-group-item list-group-item-action" v-for="teacher in teachers" :key="teacher.id" @click="getSubjectsbyTeacherId(teacher.id); getStudentsbyTeacherId(teacher.id)">
                         <b-avatar size="6rem" variant="secondary" :text="teacher.given_name.charAt(0)+teacher.last_name.charAt(0)"></b-avatar>
                         {{ teacher.given_name }} {{ teacher.last_name }}
                     </li>
                  </ul>
                     <b-collapse id="collapse-1" class="mt-2">
                         <b-card>
-                            <p class="card-text" v-for="(subject, index) in subjectsbyTeacherId" :key="index">{{ subject.subject }}</p>
 
-                            <div> {{ studentsbyTeacherId.length }} students enrolled </div>
+                           
+                            <p class="card-text" v-for="(subject, index) in subjectsbyTeacherId" :key="index">{{ subject.subject }} </p>
+                            
+                            <b-button variant="outline-info" v-b-toggle.collapse-1-inner size="sm">Show Students</b-button>
 
-                            <b-button v-b-toggle.collapse-1-inner size="sm">Show Students</b-button>
+                             <div class="float-right"> {{ studentsbyTeacherId.length }} students enrolled </div>
 
                             <b-collapse v-for="(student, index) in studentsbyTeacherId" :key="index" id="collapse-1-inner" class="mt-2">
-
                                 <b-card>
                                     {{ student.student_given_name }} {{ student.student_last_name }}
                                 </b-card>
                             </b-collapse>
+
                         </b-card>
                     </b-collapse>
             </div><br/><br/>
@@ -29,35 +31,7 @@
                 <b-table id="teacher-subjects" bordered small table-variant="info" :items="subjectsbyTeacherId"></b-table>
                 <b-table id="teacher-students" bordered small table-variant="info" :items="studentsbyTeacherId"></b-table>
             </div> -->
-            <!-- <div>
-                <b-table id="teacher-students" bordered small table-variant="info" :items="studentsbyTeacherId"></b-table>
-                 <div class="jumbotron" style="border-radius:0px;">
-                        <slot> HOW </slot>    
-                </div>
-                <h1>{{ getTeachers }}</h1>
-            </div> -->
-        </div><br/><br/>
-        
-        <!-- <v-container>
-            <v-layout row wrap>
-                <v-flex xs12 sm6 lg3 v-for="(teacher, id) in teachers" :key="id">
-                    <v-card flat class="text-xs-center ma-3">
-                        <v-card-action>
-                            <v-btn @click="openAvatarPicker" class="btn btn-danger">Change Avatar</v-btn>
-                        </v-card-action>       
-                        <v-responsive class="pt-4">
-                            <v-avatar>
-                                <img :src="person.avatar">                       
-                            </v-avatar>
-                        </v-responsive>
-                        <v-card-text>
-                            <div> {{ teacher.given_name }} </div>
-                            <div> {{ teacher.last_name }} </div>
-                        </v-card-text>
-                    </v-card>
-                </v-flex>
-            </v-layout>
-        </v-container> -->
+        </div>
     </div>
 </template>
 <script>
@@ -69,14 +43,7 @@ export default {
             teachers: [],
             subjectsbyTeacherId: [],
             studentsbyTeacherId: []
-            // team: [
-            //     { name: 'Maria', expertise: 'Vue', avatar: '/avatar-1.png'},
-            //     { name: 'Eduardo', expertise: 'Technology', avatar: '/avatar-2.png'},
-            //     { name: 'Melissa', expertise: 'Loop', avatar: '/avatar-3.png'},
-            //     { name: 'Anne', expertise: 'API', avatar: '/avatar-4.png'},
-            //     { name: 'Mike', expertise: 'Bootstrap', avatar: '/avatar-5.png'},
-            //     { name: 'Adam', expertise: 'React', avatar: '/avatar-6.png'}
-            // ]
+            // PASS PROPS grades: [] from Grades.Vue - render subject
         }
     },
 
